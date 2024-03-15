b24343bf86f1df89eea127b7f63b286c1ad82c8f
Floris Buyse <floris.buyse@student.hogent.be>
2024-03-15 16:20:09 +0100

    optimized crop_random_scales

--- scripts/crop_random_scales.py
+++ scripts/crop_random_scales.py
@@ -11,19 +11,15 @@ import torch
 IMG_PATH_RANDOM = './images/classification/random'
 MODEL_PATH_OBB = './models/BEST_m_obb.pt'
 SAVE_DIR_RANDOM = './images/cropped_scales/random'
-
-settings.update({'runs_dir': './models/runs'})
 RUNS_DIR = settings['runs_dir']
-
 OBB_PREDICT_PATH = os.path.join(RUNS_DIR, 'obb/predict')
 
-try:
-    if not os.path.exists(SAVE_DIR_RANDOM):
-        os.makedirs(SAVE_DIR_RANDOM)
-except OSError as e:
-    log(f'Error: {e}')
-    print('Something went wrong, check the log file for more information')
-
+def create_directory(directory):
+    try:
+        os.makedirs(directory, exist_ok=True)
+    except OSError as e:
+        log(f'Error: {e}')
+        print('Something went wrong, check the log file for more information')
 
 def crop_scale_random(images: list):
     model_obb = YOLO(MODEL_PATH_OBB)
@@ -33,41 +29,29 @@ def crop_scale_random(images: list):
     results_fixed = model_obb(images, conf=0.7)
     return results_fixed
 
-
 def load_image(image_path, fig_size=(50, 50), grid=False, x_ticks=30, y_ticks=10, x_rotation=0, y_rotation=0, save=False, save_path=None):
     # Load the image
     image = cv2.imread(image_path)
-
     # Convert the image from BGR to RGB (matplotlib uses RGB)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
     # Create a figure with a specific size
     plt.figure(figsize=fig_size)
-
     # Show the image
     plt.imshow(image_rgb)
-
     if grid:
         # Add grid lines to the image for easier measurement
         plt.grid(color='r', linestyle='-', linewidth=0.5)
-
         # Optionally, you can customize the ticks to match your image's scale
         plt.xticks(range(0, image_rgb.shape[1], x_ticks), rotation=x_rotation)  # Adjust the spacing as needed
         plt.yticks(range(0, image_rgb.shape[0], y_ticks), rotation=y_rotation)  # Adjust the spacing as needed
-
     if save:
-        if save_path is None:
-            plt.savefig(image_path.split('/')[-1], bbox_inches='tight', transparent=True)
-            plt.close()
-        else:
-            plt.savefig(save_path, bbox_inches='tight', transparent=True)
-            plt.close()
-
+        save_path = save_path or image_path.split('/')[-1]
+        plt.savefig(save_path, bbox_inches='tight', transparent=True)
+        plt.close()
 
 def distance(p1, p2):
     """Calculate the Euclidean distance between two points."""
     return ((p1 - p2) ** 2).sum().sqrt()
-
 
 def width_length_difference(box):
     """Calculate the width and length of a bounding box and return their difference."""
@@ -83,66 +67,58 @@ def width_length_difference(box):
     # Return the absolute difference between length and width
     return abs(length - width)
 
-
-try:
-    all_images = os.listdir(IMG_PATH_RANDOM)
-    images = [os.path.join(IMG_PATH_RANDOM, img) for img in all_images]
-
-    results = crop_scale_random(images)
-except Exception as e:
-    log(f'Error cropping images: {e}')
-    print('Something went wrong, check the log file for more information')
-
-for result in results:
-    try:
-        image_path = result.path
-        image_name = image_path.split('/')[-1]
-
-        if len(result.obb.xyxyxyxy) > 1:
-            differences = torch.tensor([width_length_difference(box) for box in result.obb.xyxyxyxy])
-            box_with_largest_difference_index = differences.argmax().item()
-            bbox = result.obb.xyxyxyxy[box_with_largest_difference_index]
-        else:
-            bbox = result.obb.xyxyxyxy[0]
-
-        # Load the image
-        image = cv2.imread(image_path)
-
-        # Define the quadrilateral
-        # check for cuda
-        if torch.cuda.is_available():
-            quadrilateral = np.array(bbox.cpu())
-        else:
-            quadrilateral = np.array(bbox)
-
-        # Compute axis aligned bounding box of the quadrilateral
-        x, y, w, h = cv2.boundingRect(quadrilateral)
-
-        # Crop the image
-        cropped_image = image[y:y + h, x:x + w]
-
-        # Save the result
-        cv2.imwrite(os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only.jpg')), cropped_image)
-
-        # Move original image
-        shutil.move(image_path, os.path.join(SAVE_DIR_RANDOM, image_name))
-
-        # Add grid to the image
-        load_image(os.path.join(SAVE_DIR_RANDOM, image_name), grid=True, x_ticks=120, y_ticks=10, x_rotation=90, y_rotation=0, save=True, save_path=os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_grid.jpg')))
-
-        # Add grid to the cropped image
+def process_results(results):
+    for result in results:
         try:
-            load_image(os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only.jpg')), grid=True, x_ticks=120, y_ticks=10, x_rotation=90, y_rotation=0, save=True, save_path=os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only_grid.jpg')))
-        except:
-            pass
+            image_path = result.path.replace('\\', '/')
+            image_name = image_path.split('/')[-1]
+            if len(result.obb.xyxyxyxy) > 1:
+                differences = torch.tensor([width_length_difference(box) for box in result.obb.xyxyxyxy])
+                box_with_largest_difference_index = differences.argmax().item()
+                bbox = result.obb.xyxyxyxy[box_with_largest_difference_index]
+            else:
+                bbox = result.obb.xyxyxyxy[0]
+
+            # Load the image
+            image = cv2.imread(image_path)
+            # Define the quadrilateral
+            quadrilateral = np.array(bbox.cpu() if torch.cuda.is_available() else bbox)
+            # Compute axis aligned bounding box of the quadrilateral
+            x, y, w, h = cv2.boundingRect(quadrilateral)
+            # Crop the image
+            cropped_image = image[y:y + h, x:x + w]
+            # Save the result
+            cv2.imwrite(os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only.jpg')), cropped_image)
+            # Move original image
+            shutil.move(image_path, os.path.join(SAVE_DIR_RANDOM, image_name))
+            # Add grid to the image
+            load_image(os.path.join(SAVE_DIR_RANDOM, image_name), grid=True, x_ticks=120, y_ticks=10, x_rotation=90, y_rotation=0, save=True, save_path=os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_grid.jpg')))
+            # Add grid to the cropped image
+            try:
+                load_image(os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only.jpg')), grid=True, x_ticks=120, y_ticks=10, x_rotation=90, y_rotation=0, save=True, save_path=os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only_grid.jpg')))
+            except:
+                pass
+        except Exception as e:
+            log(f'Error cropping images: {e}')
+            print('Something went wrong, check the log file for more information')
+
+def main():
+    create_directory(SAVE_DIR_RANDOM)
+    try:
+        all_images = os.listdir(IMG_PATH_RANDOM)
+        images = [os.path.join(IMG_PATH_RANDOM, img) for img in all_images]
+        results = crop_scale_random(images)
+        process_results(results)
     except Exception as e:
         log(f'Error cropping images: {e}')
         print('Something went wrong, check the log file for more information')
-    
-# Remove old directory
-try:
-    shutil.rmtree(OBB_PREDICT_PATH, ignore_errors=True)
-    shutil.rmtree(IMG_PATH_RANDOM, ignore_errors=True)
-except Exception as e:
-    log(f'Error removing old images: {e}')
-    print('Something went wrong, check the log file for more information')
+    # Remove old directory
+    try:
+        shutil.rmtree(OBB_PREDICT_PATH, ignore_errors=True)
+        shutil.rmtree(IMG_PATH_RANDOM, ignore_errors=True)
+    except Exception as e:
+        log(f'Error removing old images: {e}')
+        print('Something went wrong, check the log file for more information')
+
+if __name__ == '__main__':
+    main()
