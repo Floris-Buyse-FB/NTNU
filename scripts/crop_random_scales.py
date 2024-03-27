46f3c70be54f88be2649da8442ad094002e07546
Floris Buyse <floris.buyse@student.hogent.be>
2024-03-27 17:52:37 +0100

    improved the random scale measurment

--- scripts/crop_random_scales.py
+++ scripts/crop_random_scales.py
@@ -29,25 +29,25 @@ def crop_scale_random(images: list):
     results_fixed = model_obb(images, conf=0.7, verbose=False)
     return results_fixed
 
-def load_image(image_path, fig_size=(50, 50), grid=False, x_ticks=30, y_ticks=10, x_rotation=0, y_rotation=0, save=False, save_path=None):
-    # Load the image
-    image = cv2.imread(image_path)
-    # Convert the image from BGR to RGB (matplotlib uses RGB)
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # Create a figure with a specific size
-    plt.figure(figsize=fig_size)
-    # Show the image
-    plt.imshow(image_rgb)
-    if grid:
-        # Add grid lines to the image for easier measurement
-        plt.grid(color='r', linestyle='-', linewidth=0.5)
-        # Optionally, you can customize the ticks to match your image's scale
-        plt.xticks(range(0, image_rgb.shape[1], x_ticks), rotation=x_rotation)  # Adjust the spacing as needed
-        plt.yticks(range(0, image_rgb.shape[0], y_ticks), rotation=y_rotation)  # Adjust the spacing as needed
-    if save:
-        save_path = save_path or image_path.split('/')[-1]
-        plt.savefig(save_path, bbox_inches='tight', transparent=True)
-        plt.close()
+# def load_image(image_path, fig_size=(50, 50), grid=False, x_ticks=30, y_ticks=10, x_rotation=0, y_rotation=0, save=False, save_path=None):
+#     # Load the image
+#     image = cv2.imread(image_path)
+#     # Convert the image from BGR to RGB (matplotlib uses RGB)
+#     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     # Create a figure with a specific size
+#     plt.figure(figsize=fig_size)
+#     # Show the image
+#     plt.imshow(image_rgb)
+#     if grid:
+#         # Add grid lines to the image for easier measurement
+#         plt.grid(color='r', linestyle='-', linewidth=0.5)
+#         # Optionally, you can customize the ticks to match your image's scale
+#         plt.xticks(range(0, image_rgb.shape[1], x_ticks), rotation=x_rotation)  # Adjust the spacing as needed
+#         plt.yticks(range(0, image_rgb.shape[0], y_ticks), rotation=y_rotation)  # Adjust the spacing as needed
+#     if save:
+#         save_path = save_path or image_path.split('/')[-1]
+#         plt.savefig(save_path, bbox_inches='tight', transparent=True)
+#         plt.close()
 
 def distance(p1, p2):
     """Calculate the Euclidean distance between two points."""
@@ -70,7 +70,7 @@ def width_length_difference(box):
 def process_results(results):
     for result in results:
         try:
-            image_path = result.path.replace('\\', '/')
+            image_path = result.path
             image_name = image_path.split('/')[-1]
             if len(result.obb.xyxyxyxy) > 1:
                 differences = torch.tensor([width_length_difference(box) for box in result.obb.xyxyxyxy])
@@ -107,8 +107,14 @@ def main():
     try:
         all_images = os.listdir(IMG_PATH_RANDOM)
         images = [os.path.join(IMG_PATH_RANDOM, img) for img in all_images]
-        results = crop_scale_random(images)
-        process_results(results)
+        if len(images) > 50:
+            for i in range(0, len(images), 50):
+                batch = images[i:i+50]
+                results = crop_scale_random(batch)
+                process_results(results)
+        else:
+            results = crop_scale_random(images)
+            process_results(results)
     except Exception as e:
         log(f'Error cropping images: {e}')
         print('Something went wrong, check the log file for more information')
