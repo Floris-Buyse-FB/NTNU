9830fe3b2da1e236e36511f6c048d20ad9da6423
Floris Buyse <floris.buyse@student.hogent.be>
2024-03-28 18:42:22 +0100

    refactored crop random scales script

--- scripts/crop_random_scales.py
+++ scripts/crop_random_scales.py
@@ -1,58 +1,26 @@
-from ultralytics import YOLO, settings
-import os
-import shutil
-import cv2
-import matplotlib.pyplot as plt
+from packages import log, check_directory, get_image_paths, load_model, remove_old_directories, remove_old_runs, predict
+from ultralytics import settings
 import numpy as np
-from packages import log
+import shutil
 import torch
-import argparse
+import cv2
+import os
 
 # CONSTANTS
-IMG_PATH_RANDOM = './images/classification/random'
-MODEL_PATH_OBB = './models/crop_random_scale.pt'
-SAVE_DIR_RANDOM = './images/cropped_scales/random'
+IMG_PATH = './images/classification/random'
+SAVE_DIR = './images/cropped_scales/random'
+FILENAME = os.path.basename(__file__)
+
+MODEL_PATH= './models/crop_random_scale.pt'
+
 RUNS_DIR = settings['runs_dir']
-OBB_PREDICT_PATH = os.path.join(RUNS_DIR, 'obb/predict')
+PREDICT_PATH = os.path.join(RUNS_DIR, 'obb/predict')
 
-def create_directory(directory):
-    try:
-        os.makedirs(directory, exist_ok=True)
-    except OSError as e:
-        log(f'Error: {e}')
-        print('Something went wrong, check the log file for more information')
-
-def crop_scale_random(images: list):
-    model_obb = YOLO(MODEL_PATH_OBB)
-    # remove old runs
-    shutil.rmtree(os.path.join(RUNS_DIR, 'obb'), ignore_errors=True)
-    # predict images
-    results_fixed = model_obb(images, conf=0.7, verbose=False)
-    return results_fixed
-
-# def load_image(image_path, fig_size=(50, 50), grid=False, x_ticks=30, y_ticks=10, x_rotation=0, y_rotation=0, save=False, save_path=None):
-#     # Load the image
-#     image = cv2.imread(image_path)
-#     # Convert the image from BGR to RGB (matplotlib uses RGB)
-#     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#     # Create a figure with a specific size
-#     plt.figure(figsize=fig_size)
-#     # Show the image
-#     plt.imshow(image_rgb)
-#     if grid:
-#         # Add grid lines to the image for easier measurement
-#         plt.grid(color='r', linestyle='-', linewidth=0.5)
-#         # Optionally, you can customize the ticks to match your image's scale
-#         plt.xticks(range(0, image_rgb.shape[1], x_ticks), rotation=x_rotation)  # Adjust the spacing as needed
-#         plt.yticks(range(0, image_rgb.shape[0], y_ticks), rotation=y_rotation)  # Adjust the spacing as needed
-#     if save:
-#         save_path = save_path or image_path.split('/')[-1]
-#         plt.savefig(save_path, bbox_inches='tight', transparent=True)
-#         plt.close()
 
 def distance(p1, p2):
     """Calculate the Euclidean distance between two points."""
     return ((p1 - p2) ** 2).sum().sqrt()
+
 
 def width_length_difference(box):
     """Calculate the width and length of a bounding box and return their difference."""
@@ -65,70 +33,66 @@ def width_length_difference(box):
     ])
     # The length is the maximum of the four sides, and width is the second maximum (considering a rotated box)
     length, width = sides.topk(2).values
-    # Return the absolute difference between length and width
     return abs(length - width)
 
+
 def process_results(results):
-    for result in results:
-        try:
+    for result_list in results:
+        for result in result_list:
             image_path = result.path
-            image_name = image_path.split('/')[-1]
+            image_name = os.path.basename(image_path)
+
             if len(result.obb.xyxyxyxy) > 1:
                 differences = torch.tensor([width_length_difference(box) for box in result.obb.xyxyxyxy])
                 box_with_largest_difference_index = differences.argmax().item()
                 bbox = result.obb.xyxyxyxy[box_with_largest_difference_index]
             else:
                 bbox = result.obb.xyxyxyxy[0]
-
-            # Load the image
+            
             image = cv2.imread(image_path)
-            # Define the quadrilateral
             quadrilateral = np.array(bbox.cpu() if torch.cuda.is_available() else bbox)
-            # Compute axis aligned bounding box of the quadrilateral
             x, y, w, h = cv2.boundingRect(quadrilateral)
-            # Crop the image
-            cropped_image = image[y:y + h, x:x + w]
-            # Save the result
-            cv2.imwrite(os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only.jpg')), cropped_image)
-            # Move original image
-            shutil.move(image_path, os.path.join(SAVE_DIR_RANDOM, image_name))
-            # # Add grid to the image
-            # load_image(os.path.join(SAVE_DIR_RANDOM, image_name), grid=True, x_ticks=120, y_ticks=10, x_rotation=90, y_rotation=0, save=True, save_path=os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_grid.jpg')))
-            # # Add grid to the cropped image
-            # try:
-            #     load_image(os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only.jpg')), grid=True, x_ticks=120, y_ticks=10, x_rotation=90, y_rotation=0, save=True, save_path=os.path.join(SAVE_DIR_RANDOM, image_name.replace('.jpg', '_scale_only_grid.jpg')))
-            # except:
-            #     pass
-        except Exception as e:
-            log(f'Error cropping images: {e}')
-            print('Something went wrong, check the log file for more information')
+            cropped_image = image[y:y+h, x:x+w]
+            cv2.imwrite(os.path.join(SAVE_DIR, image_name.replace('.jpg', '_scale_only.jpg')), cropped_image)
+            shutil.move(image_path, os.path.join(SAVE_DIR, image_name))
 
-def main(batch_size=22):
-    create_directory(SAVE_DIR_RANDOM)
+
+def main():
     try:
-        all_images = os.listdir(IMG_PATH_RANDOM)
-        images = [os.path.join(IMG_PATH_RANDOM, img) for img in all_images]
-        if len(images) > batch_size:
-            for i in range(0, len(images), batch_size):
-                batch = images[i:i+batch_size]
-                results = crop_scale_random(batch)
-                process_results(results)
-        else:
-            results = crop_scale_random(images)
-            process_results(results)
+        check_directory(SAVE_DIR)
     except Exception as e:
-        log(f'Error cropping images: {e}')
-        print('Something went wrong, check the log file for more information')
-    # Remove old directory
+        log(f'Error creating directory: {e}', FILENAME)
+
     try:
-        shutil.rmtree(OBB_PREDICT_PATH, ignore_errors=True)
-        # shutil.rmtree(IMG_PATH_RANDOM, ignore_errors=True)
+        model = load_model(MODEL_PATH)
     except Exception as e:
-        log(f'Error removing old images: {e}')
-        print('Something went wrong, check the log file for more information')
+        log(f'Error loading model: {e}', FILENAME)
+    
+    try:
+        images = get_image_paths(IMG_PATH)
+    except Exception as e:
+        log(f'Error getting image paths: {e}', FILENAME)
+
+    try:
+        remove_old_runs('obb')
+    except Exception as e:
+        log(f'Error removing old runs: {e}', FILENAME)
+
+    try:
+        results = predict(model, images, conf=0.7, verbose=False)
+    except Exception as e:
+        log(f'Error predicting images: {e}', FILENAME)
+
+    try:
+        process_results(results)
+    except Exception as e:
+        log(f'Error processing results: {e}', FILENAME)
+        
+    try:
+        remove_old_directories([IMG_PATH, PREDICT_PATH])
+    except Exception as e:
+        log(f'Error removing directories: {e}', FILENAME)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Crop random scale images')
-    parser.add_argument('-b', '--batch_size', type=int, default=22, help='Number of images to process at once')
-    args = parser.parse_args()
-    main(args.batch_size)
+    main()
